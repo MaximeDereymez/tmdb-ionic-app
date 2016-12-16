b85475a69312f5c5b0d91d8ef16febbb51e2635d
Maxime Dereymez <dereymez.maxime@gmail.com>
2016-12-16 16:48:19 +0100

    TP3 gros commit

--- src/modules/app.states/service.states.js
+++ src/modules/app.states/service.states.js
@@ -4,19 +4,41 @@
 (function (module) {
   'use strict';
 
-  function StatesService($q, httpService, i18nService) {
+  function StatesService(
+    $q,
+    httpService, 
+    i18nService,
+    API_IMAGES_URL,
+    API_KEY
+  ) {
     var service = this;
 
     service.search = function (query) {
-      console.log(query);
-      return $q.resolve([
-        { title: 'TestMovie1', id: 1 },
-        { title: 'TestMovie2', id: 2 }
-      ]);
+      return httpService.get('/3/search/movie', {
+        language: i18nService.getLocale(),
+        api_key: API_KEY,
+        query: query
+      }).then(function (data) {
+        return data.results;
+      });
     };
 
     service.getMovie = function(id) {
-      return $q.resolve({ title: 'TestMovie', id: id});
+      return httpService.get('/3/movie/' + id, {
+        language: i18nService.getLocale(),
+        api_key: API_KEY
+      });
+    };
+
+    service.discoverMovie = function () {
+      return httpService.get('/3/discover/movie', {
+        'release_date.lte': moment().add(3, 'months').format('YYYY-MM-DD'),
+        'release_date.gte': moment().format('YYYY-MM-DD'),
+        language: i18nService.getLocale(),
+        api_key: API_KEY
+      }).then(function (data) {
+        return _.sample(data.results) || $q.reject();
+      });
     };
 
     /**
@@ -35,6 +57,8 @@
     '$q',
     'httpService',
     'i18nService',
+    'API_IMAGES_URL',
+    'API_KEY',
     StatesService
   ]);
 
